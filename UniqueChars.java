1600f8013616801b8d88e740ad0c6f41bc4ecd4a
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 20:38:15 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -1,6 +1,6 @@
 public class UniqueChars {
     public static void main(String[] args) {  
-        String str = args[0];
+        String str = args[0]; 
         System.out.println(uniqueChars(str));
     }
 
