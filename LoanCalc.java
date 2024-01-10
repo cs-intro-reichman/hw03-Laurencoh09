b4085d4ba9bc1db18be47cb24f9552168304873d
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 19:37:17 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -4,9 +4,10 @@ public class LoanCalc {
     static int iterationCounter;    
     public static void main(String[] args) {
         // Gets the loan data
-        double loan = 100000;
-        double rate = 5;
-        int n = 10;
+        double loan = Double.parseDouble(args[0]);
+        double rate = Double.parseDouble(args[1]);
+        int n = Integer.parseInt(args[2]);
+        
         System.out.println(" Loan sum = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
     
