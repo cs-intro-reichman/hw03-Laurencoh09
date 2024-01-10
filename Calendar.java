6f93c9b5fbb44e637d2b196396d86a98a369056f
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 19:29:12 +0200

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -62,7 +62,7 @@ public class Calendar {
 		}
 		 
 		private static int nDaysInMonth(int month, int year) {
-			// Replace the following statement with your code
+			
 			if(month == 4 || month == 6 || month == 9 || month ==11) return 30;
 			if(month == 1 || month == 3 || month == 4 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
 			if(month == 2){
