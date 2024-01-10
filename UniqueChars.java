46db0209b70b2395729a8c6b458f171c048ba551
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 17:21:40 +0200

    Update UniqueChars.java

--- UniqueChars.java
+++ UniqueChars.java
@@ -4,6 +4,11 @@ public class UniqueChars {
         System.out.println(uniqueChars(str));
     }
 
+    /**
+     * Returns a string which is identical to the original string, 
+     * except that all the duplicate characters are removed,
+     * unless they are space characters.
+     */
     public static String uniqueChars(String s) {
         String uniqueCharcaters = "";
 
