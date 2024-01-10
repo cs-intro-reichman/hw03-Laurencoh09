dc92826275b07d3dd3e7d4e7296d317f76d484a7
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 18:18:14 +0200

    Update Calendar1.java

--- Calendar1.java
+++ Calendar1.java
@@ -1,86 +1,74 @@
-/** 
- * Prints the calendars of all the years in the 20th century.
- */
 public class Calendar1 {	
-    // Starting the calendar on 1/1/1900
-	static int dayOfMonth = 1;   
-	static int month = 1;
-	static int year = 1900;
-	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
-	static int nDaysInMonth = 31; // Number of days in January
-	
-	/** 
-	 * Prints the calendars of all the years in the 20th century. Also prints the  
-	 * number of Sundays that occured on the first day of the month during this period.
-	 */
-	public static void main(String args[]) {
-		int debugDaysCounter = 0;
-		while (year <= 1999) { // Iterate through years until 1999
-			advance();
-			debugDaysCounter++;
-			// You can add any specific conditions to stop the loop if needed.
-		}
-		// Additional code if necessary after the loop.
-		System.out.println("Total number of days advanced: " + debugDaysCounter);
-	 }
-	
-	 // Advances the date (day, month, year) and the day-of-the-week.
-	 // If the month changes, sets the number of days in this month.
-	 // Side effects: changes the static variables dayOfMonth, month, year, dayOfWeek, nDaysInMonth.
-	 private static void advance() {
-		 dayOfMonth++;
-		 dayOfWeek = (dayOfWeek % 7) + 1;
-		 if(dayOfMonth > nDaysInMonth){
-			 month++;
-			 dayOfMonth = 1;
-			 if(month > 12){
-				 year++;
-				 month = 1;
-			 }
-			 nDaysInMonth = nDaysInMonth(month, year);
-		 }
+		// Starting the calendar on 1/1/1900
+		static int dayOfMonth = 1;   
+		static int month = 1;
+		static int year = 1900;
+		static int dayOfWeek = 2;     
+		static int nDaysInMonth = 31; 
+		static int num = 0;
+		
+		
+		public static void main(String args[]) {
+			
+			int debugDaysCounter = 0; 
+			
+			while (year < 2000) {
+						
+				if(dayOfWeek == 1 && dayOfMonth == 1) num++;
+				if(dayOfWeek == 1){
+					System.out.println(dayOfMonth + "/" + month + "/" + year + " Sunday");
+				}
+				else{
+					System.out.println(dayOfMonth + "/" + month + "/" + year);
+				}
 
-		 System.out.print(dayOfMonth + ":");
-		 System.out.print(month + ":");
-		 System.out.print(year);
-		 if (dayOfWeek == 1 && dayOfMonth == 1) {
-			 System.out.println(" - Sunday");
-		 } else {
-			 System.out.println();
-		 }
+				
+				advance();	 
+				debugDaysCounter++;
+				
+				if (false) { 
+					break;
+				}
 
-	 } 
-		 
-    // Returns true if the given year is a leap year, false otherwise.
-	private static boolean isLeapYear(int year) {
-		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-	 
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
-	public static int nDaysInMonth(int month, int year) {
-		if(month == 4 || month == 6 || month == 9 || month == 11){
-			return 30;
-		}
-		else if (month == 2){
-			if(isLeapYear(year) == true){
-				return 29;
+			}
+			System.out.println("During the 20th century, " + num + " Sundays fell on the first day of the month");
+
+		 }
+		 private static void advance() {
+			
+			if(dayOfWeek == 7) dayOfWeek = 1;
+			else dayOfWeek++;
+			if(dayOfMonth == nDaysInMonth(month, year)){
+				if(month == 12) {
+					month = 1;
+					year++;
+					dayOfMonth = 1;
+				}
+				else{
+
+month++;
+					dayOfMonth = 1;
+				}
 			}
 			else{
-				return 28;
-			}
-		}
-		else{
-			return 31;
-		}
-
+				dayOfMonth++;
+		 } 
+		 }	 
+		private static boolean isLeapYear(int year) {
 		
+			boolean isLeap;
+			isLeap = ((year % 400) == 0);
+			isLeap = isLeap || (((year % 4) == 0) && ((year % 100) != 0));
+			return isLeap;
+		}
+		private static int nDaysInMonth(int month, int year) {
+			if(month == 4 || month == 6 || month == 9 || month ==11) return 30;
+			if(month == 1 || month == 3 || month == 4 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
+			if(month == 2){
+				if(isLeapYear(year)) return 29;
+				else return 28;
+			}
+			else return 0;
+			
+		}
 	}
-}
