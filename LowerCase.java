5dec20a25e69db9fb9aac90671c6642e15d59b43
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 20:36:11 +0200

    Update LowerCase.java

--- LowerCase.java
+++ LowerCase.java
@@ -1,6 +1,6 @@
 public class LowerCase {
     public static void main(String[] args) {  
-        String str = args[0];
+        String str = args[0]; 
         System.out.println(lowerCase(str));
     }
     public static String lowerCase(String s) {
