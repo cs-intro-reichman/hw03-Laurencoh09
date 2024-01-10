d3356836d09447ff845b5bc7b728ed53ff3255f8
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-01-10 20:41:31 +0200

    Update Calendar0.java

--- Calendar0.java
+++ Calendar0.java
@@ -1,17 +1,13 @@
-/*
- * Checks if a given year is a leap year or a common year,
- * and computes the number of days in a given month and a given year. 
- */
+
 public class Calendar0 {	
 	
-	// Gets a year (command-line argument), and tests the functions isLeapYear and nDaysInMonth.
 	public static void main(String args[]) {
 		int year = Integer.parseInt(args[0]);
 		isLeapYearTest(year);
 		nDaysInMonthTest(year);
 	}
 		 
-	// Tests the isLeapYear function.
+	
 	private static void isLeapYearTest(int year) {
 		String commonOrLeap = "common";
 		if (isLeapYear(year)) {
@@ -20,7 +16,7 @@ public class Calendar0 {
 		System.out.println(year + " is a " + commonOrLeap + " year");  
 	}
 
-	// Tests the nDaysInMonth function.
+
 	private static void nDaysInMonthTest(int year) {
 		for(int month = 1; month <= 12; month ++){
 			int daysInMonth = nDaysInMonth(month, year);
@@ -29,7 +25,6 @@ public class Calendar0 {
 
 	}
 
-	// Returns true if the given year is a leap year, false otherwise.
 	public static boolean isLeapYear(int year) {
 		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
 			return true;
@@ -38,11 +33,6 @@ public class Calendar0 {
 			return false;
 		}
 	}
-	 
-	// Returns the number of days in the given month and year.
-	// April, June, September, and November have 30 days each.
-	// February has 28 days in a common year, and 29 days in a leap year.
-	// All the other months have 31 days.
 	public static int nDaysInMonth(int month, int year) {
 		if(month == 4 || month == 6 || month == 9 || month == 11){
 			return 30;
